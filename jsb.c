f98da0e5ff11e25c6ddea477eae33528313a4d26
Jeremy Bennett <jerbenne@gmail.com>
2014-03-01 22:53:46 -0500

    testing out git

---
+++ jsb.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char ** argv)
+{
+	FILE * in, * out;
+
+	if(argc == 1)
+	{
+		in = stdin; 
+		out = stdout;
+	}
+	else if( argc == 2)
+	{
+		in = fopen( argv[1], "r");
+		out = stdout;
+	}
+	else
+	{
+		in = fopen( argv[1], "r");
+		out = fopen ( argv[2], "w");
+	}
+	 
+	char c;
+	int indent = 0;
+  
+	while((c = fgetc(in)) != EOF)
+	{	
+
+	   if(c == '{') {
+	   	fprintf(out, "%c", c);
+		indent += 2;
+	        fprintf(out, "\n");
+		fprintf(out,"%*s", indent, " ");		
+	    }
+	   else if(c == '}') {
+	   	indent -= 2;
+                fprintf(out, "\n");
+		fprintf(out,"%*s", indent, " ");	
+	   	fprintf(out, "%c", c);
+	   }	
+	   else if(c == ',') {	
+	   	fprintf(out, "%c", c);
+		fprintf(out, "\n");
+		fprintf(out,"%*s", indent, " ");
+	    }
+	    else if(c == '[') {
+	   	fprintf(out, "%c", c);
+		indent += 2;		
+		fprintf(out, "\n");
+		fprintf(out,"%*s", indent, " ");
+	    }
+	    else if(c == ']') {
+	        indent -=2 ;
+	   	fprintf(out, "%c", c);
+		fprintf(out, "\n");
+		fprintf(out,"%*s", indent, " ");
+	    }
+	    else
+		fprintf(out, "%c", c);
+	}
+}
+
+
